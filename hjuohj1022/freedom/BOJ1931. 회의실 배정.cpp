0b5b7c1e39a2c6bb2d230839e25168961bf412c9
한진우 <hjuohj1022@naver.com>
2025-06-09 18:00:48 +0900

    [add] BOJ1931

---
+++ hjuohj1022/freedom/BOJ1931. 회의실 배정.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+
+	vector<pair<int, int>> meetings(N);
+	for (int i = 0; i < N; i++) {
+		cin >> meetings[i].first >> meetings[i].second;
+	}
+
+	// 종료 시간 기준으로 정렬
+	sort(meetings.begin(), meetings.end(), [](pair<int, int>& a, pair<int, int>& b) {
+		return (a.second == b.second) ? a.first < b.first : a.second < b.second;
+	});
+
+	int count = 0, lastEnd = 0;
+	for (int i = 0; i < N; i++) {
+		if (meetings[i].first >= lastEnd) {
+			count++;
+			lastEnd = meetings[i].second;
+		}
+	}
+	cout << count << endl;
+	return 0;
+}
