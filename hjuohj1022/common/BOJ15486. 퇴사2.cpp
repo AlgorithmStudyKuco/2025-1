4bf0563de16c07af337107e79c24962fac484ddb
LAPTOP-5Q4D40A6\hjuoh <hjuohj1022@naver.com>
2025-06-16 18:43:24 +0900

    [add] BOJ15486

---
+++ hjuohj1022/common/BOJ15486. 퇴사2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	int t[1500001], p[1500001], dp[1500001] = { 0 };
+	
+	for (int i = 1; i <=  N; i++) {
+		cin >> t[i] >> p[i];
+	}
+	
+	for (int i = 1; i <= N; i++) {
+		if (i + t[i] - 1 <= N) {
+			// 상담종료 시기에 맞춰서 최대값 갱신 
+			dp[i + t[i] - 1] = max(dp[i + t[i] - 1], dp[i - 1] + p[i]);
+		}
+		dp[i] = max(dp[i], dp[i - 1]);
+	}
+	
+	cout << dp[N] << endl;
+	return 0;
+ }
