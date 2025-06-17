58564b9e297baa1a9b39315c810a848c5f0127cd
LAPTOP-5Q4D40A6\hjuoh <hjuohj1022@naver.com>
2025-06-17 23:34:04 +0900

    [add] BOJ1744

---
+++ hjuohj1022/common/BOJ1744. 수묶기.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	vector<int> sequence;
+	vector<int> positive;
+	vector<int> nagative;
+
+	for (int i = 0; i < N; i++) {
+		int num;
+		cin >> num;
+		sequence.push_back(num);
+	}
+
+	// 배열을 정렬
+	sort(sequence.begin(), sequence.end());
+	
+	for (int i = 0; i < N; i++) {
+		if (sequence[i] <= 0) nagative.push_back(sequence[i]);
+		else positive.push_back(sequence[i]);
+	}
+
+	int sum = 0;
+
+	// 음수 배열에서의 최댓값을 계산
+	for (int i = 0; i < nagative.size(); i++) {
+		if (i == nagative.size() - 1 && nagative.size() % 2 == 1) {
+			sum += nagative[i];
+			break;
+		}
+
+		if (i % 2 == 0) continue;
+		else {
+			sum += nagative[i] * nagative[i - 1];
+		}
+	}
+
+	// 양수 배열에서의 최댓값을 계산
+	for (int i = positive.size() - 1; i >= 0; i--) {
+		if (i == 0 && positive.size() % 2 == 1) {
+			sum += positive[i];
+			break;
+		}
+
+		if ((positive.size() - 1 - i) % 2 == 0) continue;
+		else {
+			// 이 경우는 (1 * n) < (1 + n) 이기 때문
+			if (positive[i] == 1) sum += (1 + positive[i + 1]);
+			else sum += positive[i] * positive[i + 1];
+		}
+	}
+
+	cout << sum;
+	return 0;
+}
