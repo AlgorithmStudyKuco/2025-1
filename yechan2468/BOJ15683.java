97523c764102e485713614698a4e1df484f8732d
yechan <yechan24680@gmail.com>
2025-06-30 19:46:24 +0900

    complete BOJ15683.java

--- yechan2468/BOJ15683.java
+++ yechan2468/BOJ15683.java
@@ -6,43 +6,37 @@ import java.util.StringTokenizer;
 
 public class BOJ15683 {
     static int rowSize, columnSize;
-    static int[][] board;
-    static ArrayList<Pos> blanks, walls;
-    static ArrayList<Cctv> cctvs;
-    static final int BLANK = 0, SINGLE = 1, DUAL = 2, ORTHOGONAL = 3, TRIPLE = 4, QUAD = 5, WALL = 6;
+    static ArrayList<Cctv> cctvs = new ArrayList<>();
+    static final int OCCUPIED = -1, BLANK = 0, SINGLE = 1, DUAL = 2, ORTHOGONAL = 3, TRIPLE = 4, QUAD = 5, WALL = 6;
+    static final int[] dy = {0, 1, 0, -1};
+    static final int[] dx = {1, 0, -1, 0};
+    static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
-        getInput();
+        int[][] board = getInput();
 
-        boolean[] visited = new boolean[cctvs.size()];
+        solve(0, board);
 
+        System.out.println(answer);
     }
 
-    private static void combination(int depth, )
+    private static void solve(int cctvId, int[][] board) {
+        if (cctvId >= cctvs.size()) return;
 
-    private static void getInput() throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        rowSize = Integer.parseInt(tokenizer.nextToken());
-        columnSize = Integer.parseInt(tokenizer.nextToken());
-        board = new int[rowSize][columnSize];
-        for (int i = 0; i < rowSize; i++) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            for (int j = 0; j < columnSize; j++) {
-                int elem = Integer.parseInt(tokenizer.nextToken());
-                Pos pos = new Pos(i, j);
-                switch (elem) {
-                    case BLANK:
-                        blanks.add(pos);
-                        break;
-                    case WALL:
-                        walls.add(pos);
-                        break;
-                    default:
-                        cctvs.add(new Cctv(pos, elem));
-                }
-                board[i][j] = elem;
+        for (int j = 0; j < 4; j++) {
+            Cctv cctv = cctvs.get(cctvId);
+
+            int[][] newBoard = new int[rowSize][columnSize];
+            for (int i = 0; i < rowSize; i++) {
+                newBoard[i] = board[i].clone();
             }
+
+            cctv.watch(newBoard);
+            cctv.rotate();
+            int numBlank = countBlank(newBoard);
+            answer = Math.min(answer, numBlank);
+
+            solve(cctvId + 1, newBoard);
         }
     }
 
@@ -61,16 +55,66 @@ public class BOJ15683 {
             this.rotation++;
             this.rotation %= 4;
         }
+
+        public void watch(int[][] board) {
+            switch (type) {
+                case QUAD:
+                    watchOneDirection((rotation + 2) % 4, board);
+                case TRIPLE:
+                    watchOneDirection((rotation + 3) % 4, board);
+                case ORTHOGONAL:
+                    watchOneDirection((rotation + 1) % 4, board);
+                case SINGLE:
+                    watchOneDirection(rotation, board);
+                    break;
+                case DUAL:
+                    watchOneDirection(rotation, board);
+                    watchOneDirection((rotation + 2) % 4, board);
+            }
+        }
+
+        private void watchOneDirection(int direction, int[][] board) {
+            int currRow = pos.row, currCol = pos.col;
+
+            while (board[currRow][currCol] != WALL) {
+                currRow += dy[direction];
+                currCol += dx[direction];
+                if (currRow < 0 || currRow >= rowSize || currCol < 0 || currCol >= columnSize) break;
+                if (board[currRow][currCol] == BLANK) board[currRow][currCol] = OCCUPIED;
+            }
+        }
+    }
+    
+    private static int countBlank(int[][] board) {
+        int result = 0;
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < columnSize; j++) {
+                if (board[i][j] == BLANK) result++;
+            }
+        }
+
+        return result;
     }
 
-    static class SingleCctv extends Cctv {
-        public SingleCctv(Pos pos, int type) {
-            super(pos, type);
+    private static int[][] getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        rowSize = Integer.parseInt(tokenizer.nextToken());
+        columnSize = Integer.parseInt(tokenizer.nextToken());
+        int[][] board = new int[rowSize][columnSize];
+        for (int i = 0; i < rowSize; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            for (int j = 0; j < columnSize; j++) {
+                int elem = Integer.parseInt(tokenizer.nextToken());
+                if (elem != BLANK && elem != WALL) {
+                    cctvs.add(new Cctv(new Pos(i, j), elem));
+                }
+                board[i][j] = elem;
+            }
         }
+        answer = countBlank(board);
 
-        public void watch() {
-            pos.row
-        }
+        return board;
     }
 
     static class Pos {
