4738d5d9cc35e36434d103176c2973e3a8b94b00
yechan <yechan24680@gmail.com>
2025-07-09 14:38:40 +0900

    refactor BOJ5052.java

--- yechan2468/BOJ5052.java
+++ yechan2468/BOJ5052.java
@@ -1,38 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 public class BOJ5052 {
+    static List<String> numbers;
+    static HashMap<Character, List<String>> phoneBook;
+
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int numTestcase = Integer.parseInt(reader.readLine());
         for (int t = 0; t < numTestcase; t++) {
-            int n = Integer.parseInt(reader.readLine());
-            List<String> numbers = new ArrayList<>();
-            HashMap<Character, List<String>> phoneBook = new HashMap<>();
-            for (int i = 0; i <= 9; i++) {
-                phoneBook.put(String.valueOf(i).charAt(0), new ArrayList<>());
-            }
-            for (int i = 0; i < n; i++) {
-                String number = reader.readLine();
-                numbers.add(number);
-                phoneBook.get(number.charAt(0)).add(number);
-            }
-            numbers.sort(Comparator.comparingInt(String::length));
+            initialize(reader);
             System.out.println(solve(numbers, phoneBook) ? "YES" : "NO");
         }
     }
 
     private static boolean solve(List<String> numbers, HashMap<Character, List<String>> phoneBook) {
-        for (int i = 0; i < numbers.size(); i++) {
-            for (String number : phoneBook.get(numbers.get(i).charAt(0))) {
-                if (number.length() < numbers.get(i).length() && numbers.get(i).startsWith(number)) return false;
+        for (String number1 : numbers) {
+            for (String number2 : phoneBook.get(number1.charAt(0))) {
+                if (number2.length() < number1.length() && number1.startsWith(number2)) return false;
             }
         }
         return true;
+    }
+
+    private static void initialize(BufferedReader reader) throws IOException {
+        int n = Integer.parseInt(reader.readLine());
+        numbers = new ArrayList<>();
+        phoneBook = new HashMap<>();
+        for (int i = 0; i <= 9; i++) {
+            phoneBook.put(String.valueOf(i).charAt(0), new ArrayList<>());
+        }
+        for (int i = 0; i < n; i++) {
+            String number = reader.readLine();
+            numbers.add(number);
+            phoneBook.get(number.charAt(0)).add(number);
+        }
+        numbers.sort(Comparator.comparingInt(String::length));
     }
 }
