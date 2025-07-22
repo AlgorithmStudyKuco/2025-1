7adbd002a1740982e46c33d5216863637194de83
yechan <yechan24680@gmail.com>
2025-07-22 22:12:32 +0900

    refactor BOJ1717.java

--- yechan2468/BOJ1717.java
+++ yechan2468/BOJ1717.java
@@ -4,37 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ1717 {
-    static BufferedReader reader;
-    static int numGroup, numOperation;
     static int[] parents;
+    static final String UNION = "0";
 
     public static void main(String[] args) throws IOException {
-        initialize();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = initialize(reader);
 
-        for (int i = 0; i < numOperation; i++) {
-            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-            String command = tokenizer.nextToken();
-            int a = Integer.parseInt(tokenizer.nextToken());
-            int b = Integer.parseInt(tokenizer.nextToken());
+        for (int i = 0; i < n; i++) {
+            Operation op = getOperation(reader);
+            String command = op.command; int a = op.a, b = op.b;
 
-            if (command.equals("0")) {
+            if (command.equals(UNION)) {
                 union(a, b);
             } else {
                 System.out.println(find(a) == find(b) ? "YES" : "NO");
             }
         }
 
-    }
-
-    private static void initialize() throws IOException {
-        reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        numGroup = Integer.parseInt(tokenizer.nextToken()) + 1;
-        numOperation = Integer.parseInt(tokenizer.nextToken());
-        parents = new int[numGroup];
-        for (int i = 0; i < numGroup; i++) {
-            parents[i] = i;
-        }
     }
 
     private static void union(int a, int b) {
@@ -46,5 +33,37 @@ public class BOJ1717 {
     private static int find(int x) {
         if (parents[x] == x) return x;
         return parents[x] = find(parents[x]);
+    }
+
+    private static int initialize(BufferedReader reader) throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        int numGroup = Integer.parseInt(tokenizer.nextToken()) + 1;
+        int numOperation = Integer.parseInt(tokenizer.nextToken());
+        parents = new int[numGroup];
+        for (int i = 0; i < numGroup; i++) {
+            parents[i] = i;
+        }
+
+        return numOperation;
+    }
+
+    private static Operation getOperation(BufferedReader reader) throws IOException {
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        String command = tokenizer.nextToken();
+        int a = Integer.parseInt(tokenizer.nextToken());
+        int b = Integer.parseInt(tokenizer.nextToken());
+
+        return new Operation(command, a, b);
+    }
+
+    private static class Operation {
+        public final String command;
+        public final int a, b;
+
+        public Operation(String command, int a, int b) {
+            this.command = command;
+            this.a = a;
+            this.b = b;
+        }
     }
 }
