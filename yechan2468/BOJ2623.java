7b5c7c5d3b33570a75a7f36bbec9b2c336274bc5
yechan <yechan24680@gmail.com>
2025-07-30 15:46:20 +0900

    refactor BOJ2623.java

--- yechan2468/BOJ2623.java
+++ yechan2468/BOJ2623.java
@@ -4,30 +4,13 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class BOJ2623 {
+    static int maxNumber, numArray;
+    static ArrayList<LinkedList<Integer>> graph;
+    static int[] indegrees;
+    static boolean[] visited;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        int maxNumber = Integer.parseInt(tokenizer.nextToken());
-        int numArray = Integer.parseInt(tokenizer.nextToken());
-
-        ArrayList<LinkedList<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i < maxNumber; i++) {
-            graph.add(new LinkedList<>());
-        }
-        int[] indegrees = new int[maxNumber];
-        boolean[] visited = new boolean[maxNumber];
-
-        for (int i = 0; i < numArray; i++) {
-            tokenizer = new StringTokenizer(reader.readLine());
-            int arraySize = Integer.parseInt(tokenizer.nextToken());
-            int prev = Integer.parseInt(tokenizer.nextToken()) - 1;
-            for (int j = 0; j < arraySize - 1; j++) {
-                int curr = Integer.parseInt(tokenizer.nextToken()) - 1;
-                graph.get(prev).add(curr);
-                indegrees[curr]++;
-                prev = curr;
-            }
-        }
+        initialize();
 
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
         int cnt = 0;
@@ -50,6 +33,32 @@ public class BOJ2623 {
             System.out.println(0);
         } else {
             writer.flush();
+        }
+    }
+
+    private static void initialize() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        maxNumber = Integer.parseInt(tokenizer.nextToken());
+        numArray = Integer.parseInt(tokenizer.nextToken());
+
+        graph = new ArrayList<>();
+        for (int i = 0; i < maxNumber; i++) {
+            graph.add(new LinkedList<>());
+        }
+        indegrees = new int[maxNumber];
+        visited = new boolean[maxNumber];
+
+        for (int i = 0; i < numArray; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            int arraySize = Integer.parseInt(tokenizer.nextToken());
+            int prev = Integer.parseInt(tokenizer.nextToken()) - 1;
+            for (int j = 0; j < arraySize - 1; j++) {
+                int curr = Integer.parseInt(tokenizer.nextToken()) - 1;
+                graph.get(prev).add(curr);
+                indegrees[curr]++;
+                prev = curr;
+            }
         }
     }
 }
