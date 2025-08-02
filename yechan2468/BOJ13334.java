49baee6e65478dc1905a05af51717631269df38a
yechan <yechan24680@gmail.com>
2025-08-02 14:49:25 +0900

    refactor BOJ13334.java
    1 hr 5 mins

--- yechan2468/BOJ13334.java
+++ yechan2468/BOJ13334.java
@@ -4,35 +4,31 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class BOJ13334 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int numLines = Integer.parseInt(reader.readLine());
-        int[][] lineInput = new int[numLines][2];
-        for (int i = 0; i < numLines; i++) {
-            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-            int a = Integer.parseInt(tokenizer.nextToken());
-            int b = Integer.parseInt(tokenizer.nextToken());
-            lineInput[i][0] = Math.min(a, b);
-            lineInput[i][1] = Math.max(a, b);
-        }
+    static int numLines, length;
+    static int[][] lineInput;
 
-        int length = Integer.parseInt(reader.readLine());
+    public static void main(String[] args) throws IOException {
+        getInput();
+
         Map<Integer, List<Boolean>> lines = new HashMap<>();
         for (int i = 0; i < numLines; i++) {
             int lineStart = lineInput[i][0];
             int lineEnd = lineInput[i][1];
+
             if (lineEnd - lineStart > length) continue;
+
             lines.putIfAbsent(lineEnd - length, new ArrayList<>());
             lines.get(lineEnd - length).add(Boolean.TRUE);
             lines.putIfAbsent(lineStart + 1, new ArrayList<>());
             lines.get(lineStart + 1).add(Boolean.FALSE);
         }
 
-        int count = 0;
-        int answer = 0;
         Set<Integer> keySet = lines.keySet();
         List<Integer> keyList = new ArrayList<>(keySet);
         Collections.sort(keyList);
+
+        int count = 0;
+        int answer = 0;
         for (int index : keyList) {
             for (boolean b : lines.get(index)) {
                 count += (b ? 1 : -1);
@@ -41,5 +37,22 @@ public class BOJ13334 {
         }
 
         System.out.println(answer);
+    }
+
+    private static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        numLines = Integer.parseInt(reader.readLine());
+
+        lineInput = new int[numLines][2];
+        for (int i = 0; i < numLines; i++) {
+            StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+            int a = Integer.parseInt(tokenizer.nextToken());
+            int b = Integer.parseInt(tokenizer.nextToken());
+            lineInput[i][0] = Math.min(a, b);
+            lineInput[i][1] = Math.max(a, b);
+        }
+
+        length = Integer.parseInt(reader.readLine());
     }
 }
