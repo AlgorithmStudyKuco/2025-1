fa84d014dc4291231c0549cfc4af1d95a4e7212c
yechan <yechan24680@gmail.com>
2025-08-22 20:36:02 +0900

    refactor BOJ16934.java

--- yechan2468/BOJ16934.java
+++ yechan2468/BOJ16934.java
@@ -3,62 +3,68 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 public class BOJ16934 {
-    private static BufferedReader reader;
-    private static BufferedWriter writer;
     private static int n;
     private static HashMap<String, Integer> numberedNicknames;
     private static HashMap<Integer, HashMap<Character, HashSet<String>>> aliases, nicknames;
 
     public static void main(String[] args) throws IOException {
-        initialize();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        initialize(reader);
 
         for (int i = 0; i < n; i++) {
             String nickname = reader.readLine();
-
-            if (nicknames.get(nickname.length()).get(nickname.charAt(0)).contains(nickname)) {
-                numberNickname(nickname);
-                continue;
-            }
-            nicknames.get(nickname.length()).get(nickname.charAt(0)).add(nickname);
-
-            writer.write(getPossibleAlias(nickname) + "\n");
-
-            registerPrefixes(nickname);
+            solve(nickname, writer);
         }
 
         writer.flush();
     }
 
-    private static void numberNickname(String nickname) throws IOException {
+    private static void solve(String nickname, BufferedWriter writer) throws IOException {
+        if (isExist(nicknames, nickname)) {
+            writer.write(getNumberedNickname(nickname) + "\n");
+            return;
+        }
+        register(nicknames, nickname);
+
+        writer.write(getPossibleAlias(nickname) + "\n");
+
+        registerPrefixes(nickname);
+    }
+
+    private static String getNumberedNickname(String nickname) {
         numberedNicknames.putIfAbsent(nickname, 1);
         int number = numberedNicknames.get(nickname) + 1;
         numberedNicknames.put(nickname, number);
-        writer.write(nickname + number + "\n");
+        return nickname + number;
     }
 
     private static StringBuilder getPossibleAlias(String nickname) {
         StringBuilder alias = new StringBuilder();
         for (int j = 0; j < nickname.length(); j++) {
             alias.append(nickname.charAt(j));
-            String aliasString = alias.toString();
-            if (!aliases.get(aliasString.length()).get(aliasString.charAt(0)).contains(aliasString)) break;
+            if (!isExist(aliases, alias.toString())) break;
         }
         return alias;
     }
 
     private static void registerPrefixes(String nickname) {
-        StringBuilder alias;
-        alias = new StringBuilder();
+        StringBuilder alias = new StringBuilder();
         for (int j = 0; j < nickname.length(); j++) {
             alias.append(nickname.charAt(j));
-            String aliasString = alias.toString();
-            aliases.get(aliasString.length()).get(aliasString.charAt(0)).add(aliasString);
+            register(aliases, alias.toString());
         }
     }
 
-    private static void initialize() throws IOException {
-        reader = new BufferedReader(new InputStreamReader(System.in));
-        writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static boolean isExist(HashMap<Integer, HashMap<Character, HashSet<String>>> names, String name) {
+        return names.get(name.length()).get(name.charAt(0)).contains(name);
+    }
+
+    private static void register(HashMap<Integer, HashMap<Character, HashSet<String>>> names, String name) {
+        names.get(name.length()).get(name.charAt(0)).add(name);
+    }
+
+    private static void initialize(BufferedReader reader) throws IOException {
         n = Integer.parseInt(reader.readLine());
         aliases = new HashMap<>();
         nicknames = new HashMap<>();
