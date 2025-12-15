3ad8bf7fbad8c9d37e596e64ec802ae525972a01
yechan <yechan24680@gmail.com>
2025-12-15 11:29:38 +0900

    refactor BOJ16946.java

--- yechan2468/BOJ16946.java
+++ yechan2468/BOJ16946.java
@@ -8,27 +8,12 @@ public class BOJ16946 {
     private static int[][] board;
     private static boolean[][] visited;
     private static int[][] groups;
-    private static int[] dr = new int[]{-1, 0, 0, 1};
-    private static int[] dc = new int[]{0, -1, 1, 0};
+    private static final int[] dr = new int[]{-1, 0, 0, 1};
+    private static final int[] dc = new int[]{0, -1, 1, 0};
     private static final int WALL = -1;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
-        numRow = Integer.parseInt(tokenizer.nextToken());
-        numCol = Integer.parseInt(tokenizer.nextToken());
-        board = new int[numRow][numCol];
-        visited = new boolean[numRow][numCol];
-        for (int i = 0; i < numRow; i++) {
-            String line = reader.readLine();
-            for (int j = 0; j < numCol; j++) {
-                if (line.charAt(j) == '1') {
-                    board[i][j] = WALL;
-                    visited[i][j] = true;
-                }
-            }
-        }
-        groups = new int[numRow][numCol];
+        initialize();
 
         int groupNo = 0;
         for (int i = 0; i < numRow; i++) {
@@ -49,19 +34,7 @@ public class BOJ16946 {
         for (int i = 0; i < numRow; i++) {
             for (int j = 0; j < numCol; j++) {
                 if (board[i][j] == WALL) {
-                    int count = 1;
-                    Set<Integer> group = new HashSet<>();
-                    for (int dir = 0; dir < 4; dir++) {
-                        int nRow = i + dr[dir];
-                        int nCol = j + dc[dir];
-                        if (nRow < 0 || numRow <= nRow) continue;
-                        if (nCol < 0 || numCol <= nCol) continue;
-                        if (board[nRow][nCol] == WALL) continue;
-                        if (group.contains(groups[nRow][nCol])) continue;
-                        group.add(groups[nRow][nCol]);
-                        count += board[nRow][nCol];
-                        count %= 10;
-                    }
+                    int count = getCount(i, j);
                     writer.write(String.valueOf(count));
                 } else {
                     writer.write("0");
@@ -70,6 +43,24 @@ public class BOJ16946 {
             writer.write("\n");
         }
         writer.flush();
+    }
+
+    private static int getCount(int i, int j) {
+        int count = 1;
+        Set<Integer> group = new HashSet<>();
+        for (int dir = 0; dir < 4; dir++) {
+            int nRow = i + dr[dir];
+            int nCol = j + dc[dir];
+            if (nRow < 0 || numRow <= nRow) continue;
+            if (nCol < 0 || numCol <= nCol) continue;
+            if (board[nRow][nCol] == WALL) continue;
+            if (group.contains(groups[nRow][nCol])) continue;
+            group.add(groups[nRow][nCol]);
+            count += board[nRow][nCol];
+            count %= 10;
+        }
+        return count;
+
     }
 
     public static void dfs(int row, int col, HashSet<Position> history) {
@@ -85,6 +76,25 @@ public class BOJ16946 {
             if (visited[nRow][nCol]) continue;
             dfs(nRow, nCol, history);
         }
+    }
+
+    private static void initialize() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(reader.readLine());
+        numRow = Integer.parseInt(tokenizer.nextToken());
+        numCol = Integer.parseInt(tokenizer.nextToken());
+        board = new int[numRow][numCol];
+        visited = new boolean[numRow][numCol];
+        for (int i = 0; i < numRow; i++) {
+            String line = reader.readLine();
+            for (int j = 0; j < numCol; j++) {
+                if (line.charAt(j) == '1') {
+                    board[i][j] = WALL;
+                    visited[i][j] = true;
+                }
+            }
+        }
+        groups = new int[numRow][numCol];
     }
 
     public static class Position {
